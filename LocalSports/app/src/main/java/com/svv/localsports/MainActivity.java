f4710f468826bee3c6f76acc791a1002a148fe34
jacobost <jacobost10@hotmail.es>
2020-05-02 13:45:04 +0200

    Action bar añadido, sin funcionalidad

--- LocalSports/app/src/main/java/com/svv/localsports/MainActivity.java
+++ LocalSports/app/src/main/java/com/svv/localsports/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
--- LocalSports/app/src/main/java/com/svv/localsports/controlador/home.java
+++ LocalSports/app/src/main/java/com/svv/localsports/controlador/home.java
@@ -2,11 +2,17 @@ package com.svv.localsports.controlador;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.svv.localsports.R;
 
@@ -25,5 +31,32 @@ public class home extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.menu_main, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id==R.id.action_deporte) {
+            Toast.makeText(getActivity(), "EEEEEEE", Toast.LENGTH_SHORT).show();
+        }
+        if (id==R.id.action_fecha) {
+            Toast.makeText(getActivity(), "OOOOOOOOO", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
